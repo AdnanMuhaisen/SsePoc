a230a3c41095173857c1638e1eefaca112a82db2
adnan <adnanmohaisen609@gmail.com>
2025-08-01 15:39:23 +0300

    feat(Data): Add initial migration

---
+++ src/SsePoc.Api/Infrastructure/Data/Migrations/20250801123804_InitialCreate.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace SsePoc.Api.Infrastructure.Data.Migrations;
+
+/// <inheritdoc />
+public partial class InitialCreate : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.CreateTable(
+            name: "products",
+            columns: table => new
+            {
+                id = table.Column<int>(type: "integer", nullable: false)
+                    .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                description = table.Column<string>(type: "character varying(500)", maxLength: 500, nullable: true),
+                unit_price = table.Column<decimal>(type: "numeric(8,2)", precision: 8, scale: 2, nullable: false)
+            },
+            constraints: table =>
+            {
+                table.PrimaryKey("pk_products", x => x.id);
+            });
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropTable(
+            name: "products");
+    }
+}
